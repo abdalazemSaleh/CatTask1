eab4940c74ac3b087c4d18240010beec61b0f2fe
Abdalazem Salehm <abdalazem.saleh2@icloud.com>
2024-01-05 18:00:08 +0200

    Initial Commit

---
+++ CatTask1/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CatTask1
+//
+//  Created by Abdalazem Saleh on 2024-01-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - PROPERTYS
+
+    // MARK: - IBOUTLET
+    @IBOutlet private var phoneNumber: UITextField!
+    @IBOutlet private var password: UITextField!
+    @IBOutlet private var loginButton: UIButton!
+
+    // MARK: - VIEW LIFE CYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - FUNCIONS
+    private func configureUI() {
+        let textFields: [UITextField] = [phoneNumber, password]
+        for textField in textFields {
+            textField.InputConfigurations()
+        }
+    }
+}
+
+
+extension UITextField {
+    func InputConfigurations() {
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.2
+        self.layer.shadowRadius = 10
+    }
+}
